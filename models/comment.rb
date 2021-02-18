6c4de904b29ddb0cb802a9815ea5b7170665c388
Chong Hui <iamchoonggg@gmail.com>
2021-02-18 19:07:33 +0800

    add model validation

--- models/comment.rb
+++ models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < Sequel::Model
   many_to_one :post
+
+  def validate
+    super
+    errors.add(:name, "cannot be empty") if !name || name.empty?
+    errors.add(:body, "cannot be empty") if !body || body.empty?
+  end
 end
--- models/post.rb
+++ models/post.rb
@@ -1,3 +1,9 @@
 class Post < Sequel::Model
   one_to_many :comments
+
+  def validate
+    super
+    errors.add(:title, "cannot be empty") if !title || title.empty?
+    errors.add(:body, "cannot be empty") if !body || body.empty?
+  end
 end
